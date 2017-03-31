b649447e35ca1ab0cd137e0f8c6a8e104fcf3f16
lulublondet <maria.mlvp@gmail.com>
2017-03-31 01:19:15 -0500

    identacion en el archivo de app. js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,37 +1,35 @@
-
 var select = document.getElementById('select');
 var imagens = document.querySelectorAll('.animal');
 
 select.addEventListener('change',changeColor);
 
 function changeColor() {
-
 	if (select.value == 'original') {
-	    imagens.forEach(function(e){
- 		e.classList.remove('sepia','blanco-negro','invertir-colores');
+	   	imagens.forEach(function(e){
+ 			e.classList.remove('sepia','blanco-negro','invertir-colores');
  			})
 		}
 	else if (select.value == 'sepia'){
 		imagens.forEach(function(e){
-		e.classList.remove('blanco-negro','invertir-colores');
-		e.classList.add('sepia');
+			e.classList.remove('blanco-negro','invertir-colores');
+			e.classList.add('sepia');
 			})
 		}	
 
 	else if (select.value=='blanco-negro'){
 		imagens.forEach(function(e){
-		e.classList.remove('sepia','invertir-colores');
-		e.classList.add('blanco-negro');
+			e.classList.remove('sepia','invertir-colores');
+			e.classList.add('blanco-negro');
 			})
 		}
 
 	else {
-			imagens.forEach(function(e){
+		imagens.forEach(function(e){
 			e.classList.remove('blanco-negro','sepia');
 			e.classList.add('invertir-colores');
 			})
 		}
-}	
+	}	
 
 
 
